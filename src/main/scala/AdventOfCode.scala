9bc7857c7ad0f78b4364bc4b13fbfd76f7d5ff48
Nicolas Klose <nicolas.klose@inf.ethz.ch>
2023-12-01 15:41:49 +0100

    Day1

---
+++ src/main/scala/AdventOfCode.scala
@@ -0,0 +1,34 @@
+import scala.io.Source
+
+object AdventOfCode {
+  def main(args: Array[String]): Unit = {
+    val solver = new Day1()
+    println(solver.solve2())
+  }
+}
+
+trait LoadsInput {
+  def load(file: String): List[String] = {
+    println("Loading input")
+    val bufferedSource = Source.fromFile(s"src/main/resources/$file")
+    val lines = bufferedSource.getLines.toList
+    bufferedSource.close
+    lines
+  }
+}
+
+trait StringOps {
+  def removeLetters(): String => String = (s: String) => s.filter("1234567890".contains(_))
+
+  def replaceNumberWords(): String => String = (s: String) => s.replace("one", "on1e")
+                                                            .replace("two", "tw2o")
+                                                            .replace("three", "thr3ee")
+                                                            .replace("four", "fo4ur")
+                                                            .replace("five", "fi5ve")
+                                                            .replace("six", "si6x")
+                                                            .replace("seven", "sev7en")
+                                                            .replace("eight", "ei8ght")
+                                                            .replace("nine", "ni9ne")
+
+  def firstAndLastChar(): String => String = (s: String) => s"${s(0)}${s.last}"
+}
---
+++ src/main/scala/Day1.scala
@@ -0,0 +1,18 @@
+class Day1 extends LoadsInput with StringOps {
+
+  private val lines = load("day1.txt")
+
+
+  def solve1(): Int = {
+    finish(lines)
+  }
+
+  private def finish(input: List[String]): Int = {
+    val result = input.map(removeLetters()).map(firstAndLastChar()).map(_.toInt)
+    result.sum
+  }
+
+  def solve2(): Int = {
+    finish(lines.map(replaceNumberWords()))
+  }
+}
